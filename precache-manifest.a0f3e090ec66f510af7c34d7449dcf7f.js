bea3b5b2c77fe2a5e159c51521d3dafe6366377d
Dylan Quesada <dquesada@Dylans-MacBook-Pro.local>
2019-08-22 17:55:58 -0500

    Updates

---
+++ precache-manifest.a0f3e090ec66f510af7c34d7449dcf7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1abfb8bbfc1744a90f219a2d4a11c2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e3e8d97f244821c87a3a",
+    "url": "/clicky-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "31888ad29d9093a856ea",
+    "url": "/clicky-game/static/js/2.aeedbc18.chunk.js"
+  },
+  {
+    "revision": "e3e8d97f244821c87a3a",
+    "url": "/clicky-game/static/js/main.2954751c.chunk.js"
+  },
+  {
+    "revision": "987e0eb1147bfc0bb6d9",
+    "url": "/clicky-game/static/js/runtime~main.09c9d03f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
